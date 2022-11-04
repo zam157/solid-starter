0601fa4d368597f532eedbefd93a13491e31bed9
WT <24277775+zam157@users.noreply.github.com>
2022-11-04 16:06:39 +0800

    feat: add suid

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { createSignal } from 'solid-js'
+import Button from '@suid/material/Button'
 
 export default function Home() {
   const [count, setCount] = createSignal(0)
@@ -9,21 +10,21 @@ export default function Home() {
       <p class="mt-4">This is the home page.</p>
 
       <div class="flex items-center space-x-2">
-        <button
-          class="border rounded-lg px-2 border-gray-900"
+        <Button
+          variant="contained"
           onClick={() => setCount(count() - 1)}
         >
           -
-        </button>
+        </Button>
 
         <output class="p-10px">Count: {count}</output>
 
-        <button
-          class="border rounded-lg px-2 border-gray-900"
+        <Button
+          variant="contained"
           onClick={() => setCount(count() + 1)}
         >
           +
-        </button>
+        </Button>
       </div>
     </section>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import suidPlugin from '@suid/vite-plugin'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
     Unocss(),
+    suidPlugin(),
   ],
   server: {
     port: 3000,
