0600c7654fe7635c9d99054fb5bc67f53e3a3f15
zam157 <24277775+zam157@users.noreply.github.com>
2022-11-05 17:51:06 +0800

    feat: AppBar with md style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import type { Component } from 'solid-js'
 
+import AppBar from '@suid/material/AppBar'
+import Box from '@suid/material/Box'
+import Button from '@suid/material/Button'
+import Toolbar from '@suid/material/Toolbar'
+
 import { routes } from './routes'
 
 const App: Component = () => {
@@ -8,35 +13,27 @@ const App: Component = () => {
 
   return (
     <>
-      <nav class="bg-gray-200 text-gray-900 px-4">
-        <ul class="flex items-center">
-          <li class="py-2 px-4">
-            <Link href="/" class="no-underline hover:underline">
-              Home
-            </Link>
-          </li>
-          <li class="py-2 px-4">
-            <Link href="/about" class="no-underline hover:underline">
-              About
-            </Link>
-          </li>
-          <li class="py-2 px-4">
-            <Link href="/error" class="no-underline hover:underline">
-              Error
-            </Link>
-          </li>
-
-          <li class="text-sm flex items-center space-x-1 ml-auto">
-            <span>URL:</span>
-            <input
-              class="w-75px p-1 bg-white text-sm rounded-lg"
-              type="text"
-              readOnly
-              value={location.pathname}
-            />
-          </li>
-        </ul>
-      </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit">
+              <Link href="/" class={location.pathname === '/' ? 'underline' : 'no-underline'}>
+                Home
+              </Link>
+            </Button>
+            <Button color="inherit">
+              <Link href="/about" class={location.pathname === '/about' ? 'underline' : 'no-underline'}>
+                About
+              </Link>
+            </Button>
+            <Button color="inherit">
+              <Link href="/error" class={location.pathname === '/error' ? 'underline' : 'no-underline'}>
+                Error
+              </Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
       <main>
         <Route />
